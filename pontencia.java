61df5024aa386289929e4f09be7e48cf4adc5e4e
Ryan Rezani <cancelaryanrezani@gmail.com>
2024-09-18 21:47:43 -0300

    Add files via upload

---
+++ pontencia.java
@@ -0,0 +1,17 @@
+package retornar;
+
+public class pontencia {
+	
+
+	    public static void main(String[] args) {
+	        // metedo
+	        System.out.println("19 elevado a 7 é: " + calcularPotencia(19, 7));
+	    }
+
+	    // retorna o A ao B
+	    public static double calcularPotencia(double a, double b) {
+	        return a * a * a; 
+	    }
+	}
+
+
