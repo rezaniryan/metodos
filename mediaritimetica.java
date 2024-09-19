ae591a4eb4556a15b1fc8e1b89c9c1c1d52a1b2d
Ryan Rezani <cancelaryanrezani@gmail.com>
2024-09-18 21:37:40 -0300

    Add files via upload

---
+++ mediaritimetica.java
@@ -0,0 +1,21 @@
+package ti;
+import java.util.Scanner;
+
+public class mediaritimetica {
+	
+
+
+	    public static void main(String[] args) {
+	        // mostra a mensagem
+	        System.out.println("A média aritmética é: " + calculaMedia(6, 3, 69));
+	    }
+
+	    // faz o calculo da media e mostra o resultado
+	    public static double calculaMedia(double a, double b, double c) {
+	        
+	        return (a + b + c) / 3;
+	    }
+	}
+
+
+
