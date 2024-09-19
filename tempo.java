1a63345c186b839b19bba73412198c6b079dd450
Ryan Rezani <cancelaryanrezani@gmail.com>
2024-09-18 21:56:19 -0300

    Update tempo.java

--- tempo.java
+++ tempo.java
@@ -3,11 +3,11 @@ package tempo;
 public class tempo {
 
 	    public static void main(String[] args) {
-	        // Testando o método
-	        System.out.println(converterMinutos(213)); // Saída: 1 hora(s) e 30 minuto(s)
+	        // metodo
+	        System.out.println(converterMinutos(213)); // Saída: 3 horas e 33 minutos
 	    }
 
-	    // Método que converte minutos em horas e minutos
+	    // converte minutos em horas e minutos
 	    public static String converterMinutos(int minutos) {
 	        return (minutos / 60) + " hora(s) e " + (minutos % 60) + " minuto(s)";
 	    }
